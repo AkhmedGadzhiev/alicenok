4c3238ee846a18b46514a84790a818791ef1d6fd
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:39:17 +0300

    Update index.py

--- index.py
+++ index.py
@@ -22,59 +22,62 @@ def handler():
     intents = event['request']['nlu']['intents']
     wellcome_message = event['state']['user']['wellcome_message']
 
-    if wellcome_message == 'was' and event['session']['new']:
-        return hi_message(event)
+    ERROR_RESPONSE = {
+        'response' : {
+        'text' : 'Малыш, повтори пожалуйста ещё раз',
+        'tts' : 'Малыш, повтори пожалуйста ещё раз',
+        'end_session' : False,
+        'buttons' : [
+        {'title' : 'Назад',
+        'payload' : {},
+        'hide' : True}]},
+        'version' : '1.0'}
 
-    if event['session']['new']:
-        return wellcome_me_message()
+    try:
+        if wellcome_message == 'was' and event['session']['new']:
+            return hi_message(event)
 
-    if 'main_skills' in intents:
-        return main_skills()
-    
-    if 'skills' in intents:
-        return skills(event)
+        if event['session']['new']:
+            return wellcome_me_message()
 
-    if 'entertainments' in intents:
-        return entertainments(event)
-
-    if 'continue' in intents:
-        return alicenok()
+        if 'main_skills' in intents:
+            return main_skills()
         
-    if 'back' in intents:
-        try:
+        if 'skills' in intents:
+            return skills(event)
+
+        if 'entertainments' in intents:
+            return entertainments(event)
+
+        if 'continue' in intents:
+            return alicenok()
+            
+        if 'back' in intents:
             return back(event)
-        except KeyError:
-            return {
-            'response' : {
-            'text' : 'Малыш, повтори пожалуйста ещё раз',
-            'tts' : 'Малыш, повтори пожалуйста ещё раз',
-            'end_session' : 'false',
-            'buttons' : [
-            {'title' : 'Назад',
-            'payload' : {},
-            'hide' : 'true'}]},
-            'version' : '1.0'}
 
-    if 'alicenok_actions' in intents:
-        return alicenok_actions(event)
+        if 'alicenok_actions' in intents:
+            return alicenok_actions(event)
 
-    if 'bath' in intents:
-        return bath_handler(event)
+        if 'bath' in intents:
+            return bath_handler(event)
 
-    if 'drink' in intents:
-        return drink_handler(event)
+        if 'drink' in intents:
+            return drink_handler(event)
 
-    if 'eat' in intents:
-        return feed_handler(event)
+        if 'eat' in intents:
+            return feed_handler(event)
 
-    if 'walk' in intents:
-        return walk_handler(event)
+        if 'walk' in intents:
+            return walk_handler(event)
 
-    if 'help' in intents:
-        return alicenok_help()
+        if 'help' in intents:
+            return alicenok_help()
 
-    if 'games' in intents:
-        return make_response_error('Функция пока в стадии разработки')
+        if 'games' in intents:
+            return make_response_error('Функция пока в стадии разработки')
 
-    if intents == {}: 
-        return fallback(event)
+        if intents == {}: 
+            return fallback(event)
+
+    except KeyError:
+        return ERROR_RESPONSE
