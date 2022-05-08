456d5f35b6fc95d304f9f01e8c1ca71de1cce74c
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:38:14 +0300

    Update entertainments_skills.py

--- entertainments_skills.py
+++ entertainments_skills.py
@@ -9,24 +9,24 @@ def entertainments_skills(event):
 	'response' : {
 	'text' : 'Вот список развлечений',
 	'tts' : 'Вот список развлечений. Обрати внимание что некоторые функции пока ещё в стадии разработки.',
-	'end_session' : 'false', 
+	'end_session' : False, 
 	'card' : {
 	'type' : 'BigImage', 
 	'image_id' : '1030494/7bf1cf374ceb4edf93f7'},
 	'buttons' : [
 	{'title' : 'Включи мультик', 
 	'payload' : {}, 
-	'hide' : 'true'}, 
+	'hide' : True}, 
 	{'title' : 'Расскажи сказку', 
 	'payload' : {}, 
 	'url' : 'https://music.yandex.ru/users/yamusic-podcast/playlists/1039/?from=alice&mob=0&play=1', 
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Спой песенку', 
 	'payload' : {}, 
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад', 
 	'payload' : {'back' : 'entertainments_back'}, 
-	'hide' : 'true'}]}, 
+	'hide' : True}]}, 
 	'version' : '1.0'}  
 
 
@@ -35,29 +35,44 @@ def entertainments(event):
 	intent = event['request']['nlu']['intents']['entertainments']
 	entertainment = intent['slots']['entertainments']['value']
 
-	if entertainment == 'cartoon':
-		return on_cartoons()
-
-	elif entertainment == 'story':
-		return {
+	ERROR_RESPONSE = {
 		'response' : {
-		'text' : 'Включаю сказку',
-		'tts' : 'Включаю сказку',
-		'end_session' : 'false',
+		'text' : 'Малыш, повтори пожалуйста ещё раз',
+		'tts' : 'Малыш, повтори пожалуйста ещё раз',
+		'end_session' : False,
 		'buttons' : [
 		{'title' : 'Назад',
-		'hide' : 'true',
-		'payload' : {'back' : 'on_cartoons_back'}}]},
+		'payload' : {},
+		'hide' : True}]},
 		'version' : '1.0'}
 
-	elif entertainment == 'song':
-		return {
-		'response' : {
-		'text' : 'Функция пока в стадии разработки',
-		'tts' : 'Функция пока в стадии разработки',
-		'end_session' : 'false',
-		'buttons' : [
-		{'title' : 'Назад',
-		'hide' : 'true',
-		'payload' : {'back' : 'song_back'}}]},
-		'version' : '1.0'}
+	try:
+		if entertainment == 'cartoon':
+			return on_cartoons()
+
+		if entertainment == 'story':
+			return {
+			'response' : {
+			'text' : 'Включаю сказку',
+			'tts' : 'Включаю сказку',
+			'end_session' : False,
+			'buttons' : [
+			{'title' : 'Назад',
+			'hide' : True,
+			'payload' : {'back' : 'on_cartoons_back'}}]},
+			'version' : '1.0'}
+
+		if entertainment == 'song':
+			return {
+			'response' : {
+			'text' : 'Функция пока в стадии разработки',
+			'tts' : 'Функция пока в стадии разработки',
+			'end_session' : False,
+			'buttons' : [
+			{'title' : 'Назад',
+			'hide' : True,
+			'payload' : {'back' : 'song_back'}}]},
+			'version' : '1.0'}
+
+	except KeyError:
+		return ERROR_RESPONSEs
