1319f5a49ebb8101c653d5ff008117218d5b83fe
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:38:32 +0300

    Update feed.py

--- feed.py
+++ feed.py
@@ -11,25 +11,36 @@ def feed():
 	'buttons' : [
 	{'title' : 'Крекер',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Рыба',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Салат',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Острый перец',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'feed_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
 
 # функция обрабатывает выбор еды
 def feed_handler(event):
 	intent = event['request']['nlu']['intents']['eat']
 	eat = intent['slots']['eats']['value']
+
+	ERROR_RESPONSE = {
+		'response' : {
+		'text' : 'Малыш, повтори пожалуйста ещё раз',
+		'tts' : 'Малыш, повтори пожалуйста ещё раз',
+		'end_session' : False,
+		'buttons' : [
+		{'title' : 'Назад',
+		'payload' : {},
+		'hide' : True}]},
+		'version' : '1.0'}
 
 	dict =  {
 	'response' : {
@@ -42,39 +53,41 @@ def feed_handler(event):
 	'buttons' : [
 	{'title' : 'Крекер',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Рыба',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Салат',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Острый перец',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'feed_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
 
-	if eat == 'cracker':
-		dict['response']['text'] = 'Ммм, вкусно. Давай ещё!'
-		dict['response']['tts'] = 'Вкусно! Давай ещё!'
-		return dict
+	try:
+		if eat == 'cracker':
+			dict['response']['text'] = 'Ммм, вкусно. Давай ещё!'
+			dict['response']['tts'] = 'Вкусно! Давай ещё!'
+			return dict
 
-	elif eat == 'fish':
-		dict['response']['text'] = 'Ещё, ещё!'
-		dict['response']['tts'] = 'Ещё, ещё!'
-		return dict
+		elif eat == 'fish':
+			dict['response']['text'] = 'Ещё, ещё!'
+			dict['response']['tts'] = 'Ещё, ещё!'
+			return dict
 
-	elif eat == 'salad':
-		dict['response']['text'] = 'По-моему ты меня перекармливаешь'
-		dict['response']['tts'] = 'По-моему ты меня перекармливаешь'
-		return dict
-		
+		elif eat == 'salad':
+			dict['response']['text'] = 'По-моему ты меня перекармливаешь'
+			dict['response']['tts'] = 'По-моему ты меня перекармливаешь'
+			return dict
+			
 
-	elif eat == 'pepper':
-		dict['response']['text'] = 'Аааа! Почему так остро?'
-		dict['response']['tts'] = 'А+а+а! Почему так остро?'
-		return dict
-		
+		elif eat == 'pepper':
+			dict['response']['text'] = 'Аааа! Почему так остро?'
+			dict['response']['tts'] = 'А+а+а! Почему так остро?'
+			return dict
+	except KeyError:
+		return ERROR_RESPONSE
