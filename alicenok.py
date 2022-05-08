7b020f2276f8c709ac6613bf3a2723e3d26907d7
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:36:36 +0300

    Update alicenok.py

--- alicenok.py
+++ alicenok.py
@@ -11,17 +11,17 @@ def alicenok_wellcome_message(event):
 	'response' : {
 	'text' : 'Привет! Теперь ты мой хозяин. Меня можно накормить, напоить, выгулять, помыть. Только не забывай пожалуйста обо мне',
 	'tts' : 'Привет! Теперь ты мой хозяин. Меня можно накормить, напоить, выгулять, помыть. Только не забывай пожалуйста обо мне',
-	'end_session' : 'false',
+	'end_session' : False,
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '1540737/721d93d5c6a17a965325'},
 	'buttons' : [
 	{'title' : 'Далее',
 	'payload' : {'continue' : 'continue'},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'alicenok_wellcome_message_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'user_state_update' : {'alicenok_wellcome_message' : 'was'},
 	'version' : '1.0'}
 
@@ -31,13 +31,13 @@ def alicenok_hi_message():
 	'response' : {
 	'text' : 'Привет хозяин! Я проголодался, а ещё хочу пить и мне не помешал бы лёгкий душ после прогулки',
 	'tts' : 'Привет хозяин! Я проголодался, а ещё хочу пить и мне не помешал бы лёгкий душ после прогулки',
-	'end_session' : 'false',
+	'end_session' : False,
 	'buttons' : [
 	{'title' : 'Далее',
-	'hide' : 'true',
+	'hide' : True,
 	'payload' : {'continue' : 'alicenok_continue'}},
 	{'title' : 'Назад',
-	'hide' : 'true',
+	'hide' : True,
 	'payload' : {'back' : 'alicenok_back'}}]},
 	'version' : '1.0'}
 
@@ -48,42 +48,57 @@ def alicenok():
 	'response' : {
 	'text' : 'Вот что ты можешь',
 	'tts' : 'Вот что ты можешь',
-	'end_session' : 'false', 
+	'end_session' : False, 
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '1521359/46c76afdc205da46b21a'},
 	'buttons' : [
 	{'title' : 'Покормить',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Напоить',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Выгулять',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Помыть',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'alicenok_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
 
 
 # функция обрабатывает выбор действий для Алисёнка
 def alicenok_actions(event):
-    intent = event['request']['nlu']['intents']['alicenok_actions']
-    action = intent['slots']['actions']['value']
-    
-    if action == 'feed':
-       return feed()
+	intent = event['request']['nlu']['intents']['alicenok_actions']
+	action = intent['slots']['actions']['value']
 
-    elif action == 'drunk':
-        return drunk()
+	ERROR_RESPONSE = {
+		'response' : {
+		'text' : 'Малыш, повтори пожалуйста ещё раз',
+		'tts' : 'Малыш, повтори пожалуйста ещё раз',
+		'end_session' : False,
+		'buttons' : [
+		{'title' : 'Назад',
+		'payload' : {},
+		'hide' : True}]},
+		'version' : '1.0'}
 
-    elif action == 'walk':
-        return walk()
+	try:
+		if action == 'feed':
+			return feed()
 
-    elif action == 'wash':
-        return bath()
+		elif action == 'drunk':
+			return drunk()
+
+		elif action == 'walk':
+			return walk()
+
+		elif action == 'wash':
+			return bath()
+
+	except KeyError:
+		return ERROR_RESPONSE
