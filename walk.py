9c51f3f4c64380065c1bb105220947ba73df071f
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:40:42 +0300

    Update walk.py

--- walk.py
+++ walk.py
@@ -4,23 +4,23 @@ def walk():
 	'response' : {
 	'text' : 'Пошли гулять! Куда пойдём?',
 	'tts' : 'Пошли гулять! Куда пойдём?', 
-	'end_session' : 'false', 
+	'end_session' : False, 
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '937455/a6e48cbda4c8b165f016'},
 	'buttons' : [
 	{'title' : 'В парк',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'В сад',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'На улицу',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'walk_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
 
 # функция обрабатывает выбор места для прогулки
@@ -28,57 +28,72 @@ def walk_handler(event):
 	intent = event['request']['nlu']['intents']['walk']
 	place = intent['slots']['walk']['value']
 
+	ERROR_RESPONSE = {
+		'response' : {
+		'text' : 'Малыш, повтори пожалуйста ещё раз',
+		'tts' : 'Малыш, повтори пожалуйста ещё раз',
+		'end_session' : False,
+		'buttons' : [
+		{'title' : 'Назад',
+		'payload' : {},
+		'hide' : True}]},
+		'version' : '1.0'}
+
 	dict = {
 	'response' : {
 	'text' : None,
 	'tts' : None, 
-	'end_session' : 'false', 
+	'end_session' : False, 
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : None},
 	'buttons' : [
 	{'title' : 'В парк',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'В сад',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'На улицу',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Домой',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'walk_back'},
-	'hide' : 'true'},]},
+	'hide' : True},]},
 	'version' : '1.0'}
 
-	if place == 'park':
-		del dict['response']['buttons'][0]
-		dict['response']['text'] = 'Как здесь красиво!'
-		dict['response']['tts'] = 'Как здесь красиво!'
-		dict['response']['card']['image_id'] = '1533899/1064c48a5d0fd36a946e'
-		return dict
+	try:
+		if place == 'park':
+			del dict['response']['buttons'][0]
+			dict['response']['text'] = 'Как здесь красиво!'
+			dict['response']['tts'] = 'Как здесь красиво!'
+			dict['response']['card']['image_id'] = '1533899/1064c48a5d0fd36a946e'
+			return dict
 
 
-	elif place == 'garden':
-		del dict['response']['buttons'][1]
-		dict['response']['text'] = 'Какой же здесь всё-таки чистый воздух'
-		dict['response']['tts'] = 'Какой же здесь всё-таки чистый воздух'
-		dict['response']['card']['image_id'] = '1533899/3eab7c0af24b90d57a4b'
-		return dict
+		elif place == 'garden':
+			del dict['response']['buttons'][1]
+			dict['response']['text'] = 'Какой же здесь всё-таки чистый воздух'
+			dict['response']['tts'] = 'Какой же здесь всё-таки чистый воздух'
+			dict['response']['card']['image_id'] = '1533899/3eab7c0af24b90d57a4b'
+			return dict
 
-	elif place == 'street':
-		del dict['response']['buttons'][2]
-		dict['response']['text'] = 'Опять плитку переложили'
-		dict['response']['tts'] = 'Опять плитку переложили'
-		dict['response']['card']['image_id'] = '1533899/787f214a838f26865241'
-		return dict
+		elif place == 'street':
+			del dict['response']['buttons'][2]
+			dict['response']['text'] = 'Опять плитку переложили'
+			dict['response']['tts'] = 'Опять плитку переложили'
+			dict['response']['card']['image_id'] = '1533899/787f214a838f26865241'
+			return dict
 
-	elif place == 'home':
-		del dict['response']['buttons'][3]
-		dict['response']['text'] = 'Вот мы и дома. Спасибо что погулял со мной'
-		dict['response']['tts'] = 'Вот мы и дома. Спасибо что погулял со мной'
-		dict['response']['card']['image_id'] = '937455/a6e48cbda4c8b165f016'
-		return dict
+		elif place == 'home':
+			del dict['response']['buttons'][3]
+			dict['response']['text'] = 'Вот мы и дома. Спасибо что погулял со мной'
+			dict['response']['tts'] = 'Вот мы и дома. Спасибо что погулял со мной'
+			dict['response']['card']['image_id'] = '937455/a6e48cbda4c8b165f016'
+			return dict
+			
+	except KeyError:
+		return ERROR_RESPONSE
