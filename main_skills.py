f152822d21bc2d703308d6f34402bf2f68ab0788
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:39:40 +0300

    Update main_skills.py

--- main_skills.py
+++ main_skills.py
@@ -12,64 +12,79 @@ def main_skills():
 	'response' : {
 	'text' : 'Вот что я умею',
 	'tts' : 'Вот что я умею',
-	'end_session' : 'false',
+	'end_session' : False,
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '1521359/46c76afdc205da46b21a'},
 	'buttons' : [
 	{'title' : 'Развлечения',
 	'payload' : {'entertainments' : 'entertainments'},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Мини-игры',
 	'payload' : {'games' : 'games'},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Алисёнок',
 	'payload' : {'alicenok' : 'alicenok'},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Тестировщикам',
 	'payload' : {'back' : 'testers_back'},
 	'url' : 'https://www.youtube.com/watch?v=UmS-uFOoyA0',
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Помощь',
 	'payload' : {},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
 
 
 # функция обрабатывает выбор основных возможностей
 def skills(event):
-    payload = event['request']['payload']
-    skill = event['request']['nlu']['intents']['skills']['slots']['skills']['value']
-    entertainments = {'entertainments' : 'entertainments'}
-    games = {'games' : 'games'}
-    alicenok = {'alicenok' : 'alicenok'}
-    info_games = {'info' : 'info_games'}
-    info_entertainments = {'info' : 'info_entertainments'}
-    info_alicenok = {'info' : 'info_alicenok'}
-    alicenok_wellcome_message = event['state']['user']['alicenok_wellcome_message']
+	payload = event['request']['payload']
+	skill = event['request']['nlu']['intents']['skills']['slots']['skills']['value']
+	entertainments = {'entertainments' : 'entertainments'}
+	games = {'games' : 'games'}
+	alicenok = {'alicenok' : 'alicenok'}
+	info_games = {'info' : 'info_games'}
+	info_entertainments = {'info' : 'info_entertainments'}
+	info_alicenok = {'info' : 'info_alicenok'}
+	alicenok_wellcome_message = event['state']['user']['alicenok_wellcome_message']
 
-    if payload == entertainments:
-        return entertainments_skills(event)
-  
-    elif payload == games:
-        return games_list()
-
-    elif payload == alicenok and alicenok_wellcome_message != 'was':
-        return alicenok_wellcome_message(event)
-
-    elif payload == alicenok and alicenok_wellcome_message == 'was':
-    	return alicenok_hi_message()
-
-    elif skill == 'testers':
-    	return {
+	ERROR_RESPONSE = {
 		'response' : {
-		'text' : 'Перенаправляю на страницу YouTube',
-		'end_session' : 'false',
+		'text' : 'Малыш, повтори пожалуйста ещё раз',
+		'tts' : 'Малыш, повтори пожалуйста ещё раз',
+		'end_session' : False,
 		'buttons' : [
 		{'title' : 'Назад',
-		'hide' : 'true',
-		'payload' : {'back' : 'testers_back'}}]},
+		'payload' : {},
+		'hide' : True}]},
 		'version' : '1.0'}
 
-    elif payload == info_alicenok or info_entertainments or info_games:
-    	return alicenok_help_handler(event)
+	try:
+		if payload == entertainments:
+			return entertainments_skills(event)
+
+		if payload == games:
+			return games_list()
+
+		if payload == alicenok and alicenok_wellcome_message != 'was':
+			return alicenok_wellcome_message(event)
+
+		if payload == alicenok and alicenok_wellcome_message == 'was':
+			return alicenok_hi_message()
+
+		if skill == 'testers':
+			return {
+			'response' : {
+			'text' : 'Перенаправляю на страницу YouTube',
+			'end_session' : False,
+			'buttons' : [
+			{'title' : 'Назад',
+			'hide' : True,
+			'payload' : {'back' : 'testers_back'}}]},
+			'version' : '1.0'}
+
+		if payload == info_alicenok or info_entertainments or info_games:
+			return alicenok_help_handler(event)
+
+	except KeyError:
+		return ERROR_RESPONSE
