35b54e88ce0515007729ddce0d986ace179cc10d
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:40:02 +0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -4,7 +4,7 @@ def make_response(text):
 	'response' : {
 	'text' : text, 
 	'tts' : text,
-	'end_session' : 'false'},
+	'end_session' : False},
 	'version' : '1.0'}
 
 
@@ -13,10 +13,10 @@ def make_response_error(text):
 	'response' : {
 	'text' : text, 
 	'tts' : text,
-	'end_session' : 'false',
+	'end_session' : False,
 	'buttons' : [
 	{'title' : 'Назад',
-	'hide' : 'true',
+	'hide' : True,
 	'payload' : {'back' : 'games_back'}}]}, 
 	'version' : '1.0'}
 
@@ -26,10 +26,10 @@ def wellcome_me_message():
 	'response' : {
 	'text' : 'Привет малыш! Я Алисёнок. Теперь я твой проводник в безграничном мире знаний. Чтобы узнать что я умею, спроси: "Что ты умеешь?"',
 	'tts' : 'Привет малыш! Я Алисёнок. Теперь я твой проводник в безграничном мире знаний. Чтобы узнать что я умею, спроси: "Что ты умеешь?"',
-	'end_session' : 'false',
+	'end_session' : False,
 	'buttons' : [
 	{'title' : 'Что ты умеешь',
-	'hide' : 'true',
+	'hide' : True,
 	'payload' : {}}]},
 	'user_state_update' : {
 	'wellome_message' : 'was'},
@@ -42,10 +42,10 @@ def hi_message(event):
 	'response' : {
 	'text' : 'Привет малыш! Ну что продолжим?',
 	'tts' : 'Привет малыш! Ну что продолжим?',
-	'end_session' : 'false',
+	'end_session' : False,
 	'buttons' : [
 	{'title' : 'Да',
-	'hide' : 'true',
+	'hide' : True,
 	'payload' : {}}]},
 	'version' : '1.0'}
 
@@ -57,12 +57,12 @@ def fallback(event):
 	'response' : {
 	'text' : 'Малыш, повтори пожалуйста ещё раз',
 	'tts' : 'Малыш, повтори пожалуйста ещё раз',
-	'end_session' : 'false',  
+	'end_session' : False,  
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '1652229/5a40ea676c1c9795c917'},
 	'buttons' : [
 	{'title' : 'Назад',
 	'payload' : {'back' : 'fallback_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
