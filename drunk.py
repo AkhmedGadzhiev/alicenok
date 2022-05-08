113d2f40987c1a20ca717a97895fb0f5d9506300
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:38:01 +0300

    Update drunk.py

--- drunk.py
+++ drunk.py
@@ -4,26 +4,26 @@ def drunk():
 	'response' : {
 	'text' : 'Вот меню напитков', 
 	'tts' : 'Вот меню напитков', 
-	'end_session' : 'false',
+	'end_session' : False,
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '937455/dc409d8246ba98c0713b'},
 	'buttons' : [
 	{'title' : 'Апельсиновый фреш',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Молоко',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Вода',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Смузи',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'drunk_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
 
 # функция обрабатывает выбор напитка
@@ -31,53 +31,66 @@ def drink_handler(event):
 	intent = event['request']['nlu']['intents']['drink']
 	drink = intent['slots']['drinks']['value']
 
+	ERROR_RESPONSE = {
+		'response' : {
+		'text' : 'Малыш, повтори пожалуйста ещё раз',
+		'tts' : 'Малыш, повтори пожалуйста ещё раз',
+		'end_session' : False,
+		'buttons' : [
+		{'title' : 'Назад',
+		'payload' : {},
+		'hide' : True}]},
+		'version' : '1.0'}
+
 	dict = {
 	'response' : {
 	'text' : None,
 	'tts' : None,
-	'end_session' : 'false',
+	'end_session' : False,
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '937455/dc409d8246ba98c0713b'},
 	'buttons' : [
 	{'title' : 'Апельсиновый фреш',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Молоко',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Вода',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Смузи',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'drunk_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
 
-	if drink == 'fresh':
-		dict['response']['text'] = 'Ммм, вкусно. Давай ещё!'
-		dict['response']['tts'] = 'Вкусно! Давай ещё!'
-		return dict
-		
+	try:
+		if drink == 'fresh':
+			dict['response']['text'] = 'Ммм, вкусно. Давай ещё!'
+			dict['response']['tts'] = 'Вкусно! Давай ещё!'
+			return dict
+			
 
-	elif drink == 'water':
-		dict['response']['text'] = 'Вода - это источник жизни'
-		dict['response']['tts'] = 'Вода - это источник жизни'
-		return dict
-		
+		elif drink == 'water':
+			dict['response']['text'] = 'Вода - это источник жизни'
+			dict['response']['tts'] = 'Вода - это источник жизни'
+			return dict
+			
 
-	elif drink == 'milk':
-		dict['response']['text'] = 'Люблю молоко!'
-		dict['response']['tts'] = 'Люблю молоко!'
-		return dict
-		
+		elif drink == 'milk':
+			dict['response']['text'] = 'Люблю молоко!'
+			dict['response']['tts'] = 'Люблю молоко!'
+			return dict
+			
 
-	elif drink == 'smoothies':
-		dict['response']['text'] = 'Фуу! Какая-то жижа'
-		dict['response']['tts'] = 'Фуу! Какая-то жижа'
-		return dict
-		
+		elif drink == 'smoothies':
+			dict['response']['text'] = 'Фуу! Какая-то жижа'
+			dict['response']['tts'] = 'Фуу! Какая-то жижа'
+			return dict
 
+	except KeyError:
+		return ERROR_RESPONSE
