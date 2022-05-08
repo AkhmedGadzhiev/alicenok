5f17dd5ded0bbda31cf98019da12829db5a199c4
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-08 21:37:36 +0300

    Update bathroom.py

--- bathroom.py
+++ bathroom.py
@@ -4,26 +4,26 @@ def bath():
 	'response' : {
 	'text' : 'Ура! Люблю мыться',
 	'tts' : 'Ура! Люблю мыться',
-	'end_session' : 'false', 
+	'end_session' : False, 
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '937455/a689089e088a10118d39'},
 	'buttons' : [
 	{'title' : 'Шампунь',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Гель для душа',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Бальзам для шёрстки',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Смыть',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'bathroom_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
 
 # функция обрабатывает выбор банных пренадлежностей
@@ -31,48 +31,63 @@ def bath_handler(event):
 	intent = event['request']['nlu']['intents']['bath']
 	bath = intent['slots']['bath']['value']
 
+	ERROR_RESPONSE = {
+		'response' : {
+		'text' : 'Малыш, повтори пожалуйста ещё раз',
+		'tts' : 'Малыш, повтори пожалуйста ещё раз',
+		'end_session' : False,
+		'buttons' : [
+		{'title' : 'Назад',
+		'payload' : {},
+		'hide' : True}]},
+		'version' : '1.0'}
+
 	dict = {
 	'response' : {
 	'text' : None, 
 	'tts' : None,
-	'end_session' : 'false',
+	'end_session' : False,
 	'card' : {
 	'type' : 'BigImage',
 	'image_id' : '937455/a689089e088a10118d39'},
 	'buttons' : [
 	{'title' : 'Шампунь',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Гель для душа',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Бальзам для шёрстки',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Смыть',
 	'payload' : {},
-	'hide' : 'true'},
+	'hide' : True},
 	{'title' : 'Назад',
 	'payload' : {'back' : 'bathroom_back'},
-	'hide' : 'true'}]},
+	'hide' : True}]},
 	'version' : '1.0'}
+	
+	try:
+		if bath == 'shampoo':
+			dict['response']['text'] = 'Буль, буль'
+			dict['response']['tts'] = 'Буль, буль'
+			return dict
 
-	if bath == 'shampoo':
-		dict['response']['text'] = 'Буль, буль'
-		dict['response']['tts'] = 'Буль, буль'
-		return dict
+		elif bath == 'gel':
+			dict['response']['text'] = 'Холодненький какой'
+			dict['response']['tts'] = 'Холодненький какой'
+			return dict
 
-	elif bath == 'gel':
-		dict['response']['text'] = 'Холодненький какой'
-		dict['response']['tts'] = 'Холодненький какой'
-		return dict
+		elif bath == 'balm':
+			dict['response']['text'] = 'Намыливай хорошенько'
+			dict['response']['tts'] = 'Намыливай хорошенько'
+			return dict
 
-	elif bath == 'balm':
-		dict['response']['text'] = 'Намыливай хорошенько'
-		dict['response']['tts'] = 'Намыливай хорошенько'
-		return dict
+		elif bath == 'wash_off':
+			dict['response']['text'] = 'Ой хорошо'
+			dict['response']['tts'] = 'Ой хорошо'
+			return dict
 
-	elif bath == 'wash_off':
-		dict['response']['text'] = 'Ой хорошо'
-		dict['response']['tts'] = 'Ой хорошо'
-		return dict
+	except KeyError:
+		return ERROR_RESPONSE
