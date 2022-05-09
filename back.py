e6ed3d9ad463b3e1566ae28302a43a8d52551f17
AkhmedGadzhiev <105142692+AkhmedGadzhiev@users.noreply.github.com>
2022-05-09 13:24:41 +0300

    Update back.py

--- back.py
+++ back.py
@@ -8,61 +8,61 @@ from skills.alicenok.alicenok import *
 
 # функция отправляет назад
 def back(event):
-	payload = event['request']['payload']['back']
+  payload = event['request']['payload']['back']
 
-    ERROR_RESPONSE = {
-        'response' : {
-        'text' : 'Малыш, повтори пожалуйста ещё раз',
-        'tts' : 'Малыш, повтори пожалуйста ещё раз',
-        'end_session' : False,
-        'buttons' : [
-        {'title' : 'Назад',
-        'payload' : {},
-        'hide' : True}]},
-        'version' : '1.0'}
+  ERROR_RESPONSE = {
+    'response' : {
+    'text' : 'Малыш, повтори пожалуйста ещё раз',
+    'tts' : 'Малыш, повтори пожалуйста ещё раз',
+    'end_session' : False,
+    'buttons' : [
+    {'title' : 'Назад',
+    'payload' : {},
+    'hide' : True}]},
+    'version' : '1.0'}
 
-	try:
-		if payload == 'alicenok_help_back':
-			return main_skills()
+  try:
+    if payload == 'alicenok_help_back':
+      return main_skills()
 
-		if payload == 'entertainments_back':
-			return main_skills()
+    if payload == 'entertainments_back':
+      return main_skills()
 
-		if payload == 'on_cartoons_back':
-			return entertainments_skills(event)
+    if payload == 'on_cartoons_back':
+      return entertainments_skills(event)
 
-		if payload == 'games_list_back':
-			return main_skills()
+    if payload == 'games_list_back':
+      return main_skills()
 
-		if payload == 'alicenok_wellcome_message_back':
-			return main_skills()
+    if payload == 'alicenok_wellcome_message_back':
+      return main_skills()
 
-		if payload == 'testers_back':
-			return main_skills()
+    if payload == 'testers_back':
+      return main_skills()
 
-		if payload == 'song_back':
-			return entertainments_skills(event)
+    if payload == 'song_back':
+      return entertainments_skills(event)
 
-		if payload == 'fallback_back':
-			return main_skills()
+    if payload == 'fallback_back':
+      return main_skills()
 
-		if payload == 'alicenok_back':
-			return main_skills()
+    if payload == 'alicenok_back':
+      return main_skills()
 
-		if payload == 'feed_back':
-			return alicenok()
+    if payload == 'feed_back':
+      return alicenok()
 
-		if payload == 'drunk_back':
-			return alicenok()
+    if payload == 'drunk_back':
+      return alicenok()
 
-		if payload == 'walk_back':
-			return alicenok()
+    if payload == 'walk_back':
+      return alicenok()
 
-		if payload == 'bathroom_back':
-			return alicenok()
+    if payload == 'bathroom_back':
+      return alicenok()
 
-		if payload == 'games_back':
-			return games_list()
-			
-	except KeyError:
-		return ERROR_RESPONSE
+    if payload == 'games_back':
+      return games_list()
+      
+  except KeyError:
+    return ERROR_RESPONSE
